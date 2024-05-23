9bd102ff6e9901dc9684b4912c620a9f7a84cd88
ez2say <llityka1@yandex.ru>
2024-05-23 19:28:49 +0300

    SECOND COMMIT

--- hw_3/1/main_3-1.py
+++ hw_3/1/main_3-1.py
@@ -14,7 +14,41 @@ class Spell:
     def get_type(self):
         return f'Тип заклинания {self._type}'
 
+    def get_description(self):
+        return (f'Заклинание --*{self._name}*-- имеет эффект :'
+                f'\n{self._description}')
 
+    def set_name(self,new_name:str):
+        if not isinstance(new_name, str):
+            raise TypeError
+        else:
+            self._name = new_name
+            print(f'Название заклинания сменено на {self._name}')
+
+    def set_difficult(self, new_dfficult:int):
+        if not isinstance(new_dfficult, int):
+            raise TypeError
+        else:
+            if new_dfficult >= 1 and new_dfficult <= 10:
+                self._difficult = new_dfficult
+                print(f'Сложность заклинания {self._name} изменена на {self._difficult}')
+            else:
+                raise ValueError
+
+    def set_type(self, new_type:str):
+        if not isinstance(new_type, str):
+            raise TypeError
+        else:
+            self._type = new_type
+            print(f'Тип заклинания {self._name} изменен на {self._type}')
+
+    def set_description(self, new_desc:str):
+        if not isinstance(new_desc, str):
+            raise TypeError
+        else:
+            self._description = new_desc
+            print(f'Теперь описание способности {self._name} -->'
+                  f'\n{self._description}')
 
     def __str__(self):
         return f"""
@@ -43,7 +77,7 @@ class Wizard:
         return f'Уровень магической силы у волшебника {self._name} >>> {self._spellforce}'
 
     def get_spellbook(self):
-        return f"Заклинания: {'\n'.join(str(spell) for spell in self._spellbook)}"
+        return f"Заклинания>>>> {'\n'.join(str(spell) for spell in self._spellbook)}"
 
     def get_status(self):
         return f'Статус волшебника {self._name} >>> {self._status}'
@@ -55,15 +89,6 @@ class Wizard:
             self._faculty = faculty
             print(f'Факультет сменён на: {self._faculty}')
 
-    def set_spellforce(self, spellforce:int):
-        if not isinstance(spellforce,int):
-            raise TypeError
-        else:
-            if not spellforce >= 0:
-                raise ValueError
-            else:
-                self._spellforce = spellforce
-                print(f'Теперь сила волшебника: {self._spellforce}')
 
     def set_status(self, status:str):
         self._status = status
@@ -78,11 +103,14 @@ class Wizard:
         print(f'Заклинание {spell._name} удалено из списка заклинаний.')
 
     def increase_spellforce(self, amount:int):
-        if not amount >= 0:
-            raise ValueError
+        if not isinstance(amount, int):
+            raise TypeError
         else:
-            self._spellforce = amount
-            print(f'Сила заклинаний теперь: {self._spellforce}')
+            if not amount >= 0:
+                raise ValueError
+            else:
+                self._spellforce = amount
+                print(f'Теперь сила волшебника: {self._spellforce}')
 
     def __str__(self):
         return f"""
@@ -94,103 +122,69 @@ class Wizard:
                 
                """
 
-
-
-
-
 class Program:
 
     @staticmethod
     def main():
 #####################################################ТАНЯ_ГРОТТЕР#######################################################
+
         new_wizard = Wizard('Никитос Пылесос', 'Нормальный-такой', 666, [], 'В Хогвартсе')
         bird_tornado = Spell('Птичья буря',5, 'Наносит урон', 'Вихрь состояший из тысяч птиц начинает кружить вокруг цели,нанося урон ценой жизни')
         healing_leaf = Spell('Лечащий лист', 3, 'Лечение', 'Призывает с близжайшей обочины лист подорожника,вроде лечит даже')
-        while True:
-            chs_first = int(input('[1]Действия с объектом класса волшебником'
-                                  '\n[2]Действия с объектом класса заклинаний'))
-            if chs_first == 1:
-                chs = int(input('[1]<<< Узнать имя волшебника'
-                                '\n[2]<<< Узнать факультет волшебника'
-                                '\n[3]<<< Узнать уровень магической силы волшебника'
-                                '\n[4]<<< Показать список заклинаний волшебника'
-                                '\n[5]<<< Показать статус волшебника'
-                                '\n[6]<<< FULL_INFO'
-                                '\n>>> '))
-                if chs == 1:
-                    print(new_wizard.get_name())
-                elif chs == 2:
-                    print(new_wizard.get_faculty())
-                    while True:
-                        chs_faculty = int(input('[1]Изменить факультет'
-                                             '\n[2]Назад'
-                                             '\n>>> '))
-                        if chs_faculty == 1:
-                            faculty = input('Введите название факультета: ')
-                            new_wizard.set_faculty(faculty)
-                        elif chs_faculty == 2:
-                            break
-                        else:
-                            raise ValueError
-                elif chs == 3:
-                    print(new_wizard.get_spellforce())
-                    while True:
-                        chs_force = int(input('[1]Изменить силу волшебника'
-                                              '[2]\nНазад'
-                                              '\n>>>'))
-                        if chs_force == 1:
-                            spellforce = int(input('Введите новое значение силы: '))
-                            new_wizard.set_spellforce(spellforce)
-                        elif chs_force == 2:
-                            break
-                        else:
-                            raise ValueError or TypeError
-                elif chs == 4:
-                    print(new_wizard.get_spellbook())
-                    while True:
-                        chs_spell = int(input('[1]Добавить заклинание'
-                                              '\n[2]Удалить заклинание'
-                                              '\n[3]Назад'
-                                              '\n>>> '))
-                        if chs_spell == 1:
-                            spell = input('Какое заклинание добавить [Птичья буря, Лечащий лист]: ')
-                            if spell == 'Птичья буря':
-                                new_wizard.add_spell(bird_tornado)
-                            elif spell == 'Лечащий лист':
-                                new_wizard.add_spell(healing_leaf)
-                            else:
-                                raise ValueError
-                        elif chs_spell == 2:
-                            spell = str(input(f'Какое заклинание удалить:  {new_wizard.get_spellbook()}'))
-                            if spell == 'Птичья буря':
-                                new_wizard.remove_spell(bird_tornado)
-                            elif spell == 'Лечащий лист':
-                                new_wizard.remove_spell(healing_leaf)
-                            else:
-                                raise ValueError
-                        else:
-                            break
-                elif chs == 5:
-                    print(new_wizard.get_status())
-                    chs_status = int(input('[1]Сменить статус'
-                                           '\n[2]Назад'
-                                           '\n>>>'))
-                    while True:
-                        if chs_status == 1:
-                            status = input('Введите новый статус(В Хогвартсе/Выпущен):'
-                                               '\n>>> ')
-                            new_wizard.set_status(status)
-                            break
-                        elif chs_status == 2:
-                            break
-                        else:
-                            continue
-                elif chs == 6:
-                    print(new_wizard)
-                else:
-                    raise ValueError or TypeError
-            elif chs_first == 2:
-                chs_spells = int(input('[1]'))
+
+#############GET_методы объекта класса Wizard########################################################################
+
+        new_wizard.get_name()
+        new_wizard.get_status()
+        new_wizard.get_spellbook()
+        new_wizard.get_faculty()
+        new_wizard.get_spellforce()
+
+############SET_методы объекта класса Wizard#########################################################################
+
+        status = 'Выпущен'
+        new_wizard.set_status(status)
+        faculty = 'Другой, но тоже ничего такой,пойдет'
+        new_wizard.set_faculty(faculty)
+
+##########OTHER_методы объекта класса Wizard##########################################################################
+
+        new_wizard.add_spell(bird_tornado)
+        new_wizard.add_spell(healing_leaf)
+        new_wizard.get_spellbook()
+        new_wizard.remove_spell(bird_tornado)
+        spelforce = 4
+        new_wizard.increase_spellforce(spelforce)
+        print(new_wizard)
+
+#################################################АБРАКАДАБРА############################################################
+###########GET_методы объекта класаса SPELL#############################################################################
+
+        bird_tornado.get_name()
+        bird_tornado.get_type()
+        bird_tornado.get_difficult()
+        bird_tornado.get_description()
+
+###########SET_методы объекта класса SPELL##############################################################################
+
+        print(bird_tornado) #Типа шо было вначале
+        new_name = 'Ледяной_дождь_из_нарвалов'
+        bird_tornado.set_name(new_name)
+        new_type = 'BFG9000_Только_дебафф'
+        bird_tornado.set_type(new_type)
+        new_diff = 7
+        bird_tornado.set_difficult(new_diff)
+        new_desc = 'Сверху падает рыба,больно,но не смертельно'
+        bird_tornado.set_description(new_desc)
+        print(bird_tornado) #И конечные изменения
+########################################################################################################################
+###########################################ЗЛОЕ_КОЛДУНСТВО ЗАКОНЧИЛОСЬ#################################################
+
+
+
+
+
+
 
 
 
